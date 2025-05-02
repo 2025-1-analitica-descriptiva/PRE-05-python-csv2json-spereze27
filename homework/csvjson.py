82e235da5de89b503e58251c68df9db5357d31c4
Simon perez <spereze@unal.edu.co>
2025-05-02 14:49:00 -0500

    Tarea completa 2

--- homework/csvjson.py
+++ homework/csvjson.py
@@ -7,7 +7,7 @@ from nicegui import ui
 
 def convert_csv_2_json(input_file):
     """Converts a CSV file to a JSON file"""
-    input_file="files/"+input_file
+    #input_file="files/"+input_file
 
     output_file = input_file.replace(".csv", ".json")
     data = []
